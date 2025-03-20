9b38654daad785504112f2ab8f6d6a6c8c1df378
BirsuYilmaz <ceren.birsu.yilmaz@gmail.com>
2025-03-20 15:34:58 +0300

    Implemented enemy controller

--- Counting Prototype/Assets/Scripts/EnemyController.cs
+++ Counting Prototype/Assets/Scripts/EnemyController.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+    private GameObject player;
+    private GameObject ball;
+    private GameObject goalPost;
+    private Rigidbody rb;
+    private bool isNextTo = false;
+    private float speed = 3.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        player = GameObject.Find("Player");
+        ball = GameObject.Find("Ball");
+        goalPost = GameObject.Find("GoalPost1");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        MoveEnemy();
+    }
+
+    void OnCollisionEnter(Collision other){
+        if(other.gameObject.CompareTag("Ball")){
+            isNextTo = true;
+        }
+        //if player is on the way go to another direction
+    }
+
+    void MoveEnemy(){
+        if(!isNextTo){
+            Vector3 lookDir = (ball.transform.position-transform.position).normalized;
+            rb.AddForce(lookDir * speed);
+        }
+        else{
+            Vector3 lookDir = (goalPost.transform.position-transform.position).normalized;
+            rb.AddForce(lookDir * speed);
+        }
     }
 }
--- Counting Prototype/Assets/Scripts/PlayerMovement.cs
+++ Counting Prototype/Assets/Scripts/PlayerMovement.cs
@@ -8,10 +8,13 @@ public class PlayerMovement : MonoBehaviour
     private float horizontalInput;
     private float verticalInput;
     private float speed = 10f;
+
+    private GameObject camera;
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        camera = GameObject.Find("Main Camera");
     }
 
     // Update is called once per frame
@@ -20,10 +23,10 @@ public class PlayerMovement : MonoBehaviour
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
         if(horizontalInput != 0){
-            playerRb.velocity = (Vector3.right*horizontalInput*speed);
+            playerRb.velocity = camera.transform.TransformDirection(Vector3.right*horizontalInput*speed);
         }
         if(verticalInput != 0){
-            playerRb.velocity = (Vector3.forward*verticalInput*speed);
+            playerRb.velocity = camera.transform.TransformDirection(Vector3.forward*verticalInput*speed);
         }
 
     }
